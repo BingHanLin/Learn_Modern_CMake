7007f869cc8b3dc63c3bcbf5821ed1160207b9cc
Bing Han Lin <jaguar3096@gmail.com>
2020-12-20 23:16:55 +0800

    add importing/exporting example

---
+++ 04_Importing-Exporting/mathLib/include/math.hpp
@@ -0,0 +1,7 @@
+#ifndef MATHLIB_H
+#define MATHLIB_H
+
+int add(int, int);
+double add(double, double);
+
+#endif
---
+++ 04_Importing-Exporting/mathLib/src/math.cpp
@@ -0,0 +1,11 @@
+#include "math.hpp"
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+double add(double a, double b)
+{
+    return a + b;
+}
